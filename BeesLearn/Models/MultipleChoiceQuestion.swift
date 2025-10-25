f94196efb0f1f8ed4d3d5d506c75831dcfbe15e9
Hoangdus <71692001+Hoangdus@users.noreply.github.com>
2025-10-25 08:58:15 +0700

    Add MultipleChoiceQuestion Model

---
+++ BeesLearn/Models/MultipleChoiceQuestion.swift
@@ -0,0 +1,23 @@
+//
+//  MultipleChoiceQuestion.swift
+//  BeesLearn
+//
+//  Created by HoangDus on 25/10/25.
+//
+
+import Foundation
+
+final class MultipleChoiceQuestion: Question{
+    var question: String
+    var possibleAnswers: [String]
+    
+    init(question: String, content: String, answer: String, possiableAnswers: [String]) {
+        self.question = question
+        self.possibleAnswers = possiableAnswers
+        self.possibleAnswers.append(answer)
+        self.possibleAnswers.shuffle()
+        super.init(content: content, answer: answer)
+    }
+    
+    
+}
