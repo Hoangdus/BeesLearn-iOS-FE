a9ef43a16ce5a4b94b6facd868169a49f8841323
Hoangdus <71692001+Hoangdus@users.noreply.github.com>
2025-11-23 17:14:56 +0700

    Add codingkeys to Question.swift

--- BeesLearn/Models/Question.swift
+++ BeesLearn/Models/Question.swift
@@ -7,10 +7,15 @@
 
 import Foundation
 
-class Question: Identifiable{
+class Question: Identifiable, Decodable{
     var id = UUID()
     var content: String
     var answer: String
+    
+    enum CodingKeys: String, CodingKey{
+        case content = "content"
+        case answer = "answer"
+    }
     
     init(content: String, answer: String = "") {
         self.content = content
