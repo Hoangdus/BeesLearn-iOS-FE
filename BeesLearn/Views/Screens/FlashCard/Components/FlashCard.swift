2a2dc15f020ad91abfdc2d37f21691b19322397d
Hoangdus <71692001+Hoangdus@users.noreply.github.com>
2025-12-06 06:47:48 +0700

    Add flashcard view

---
+++ BeesLearn/Views/Screens/FlashCard/Components/FlashCard.swift
@@ -0,0 +1,18 @@
+//
+//  FlashCard.swift
+//  BeesLearn
+//
+//  Created by HoangDus on 6/12/25.
+//
+
+import SwiftUI
+
+struct FlashCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FlashCard()
+}
